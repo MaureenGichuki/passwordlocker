123d1655f6bbc180505660ada7266aa1379daea6
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-25 05:38:22 +0300

    "functions to run foreach case and test init(self)

--- user_test.py
+++ user_test.py
@@ -1,5 +1,5 @@
 import unittest
-from user import user
+from user import User
 
 class Usertest(unittest.TestCase):
 
@@ -7,4 +7,21 @@ class Usertest(unittest.TestCase):
     this class test each case from the user class
 
     """
+def setUp(self):
 
+    """
+
+    the method setUp runs before each case in the user class
+
+    """
+    self.newuser= User("maureen123","hey123")
+
+def inittest(self):
+
+    """
+    test to confirm if the object user has been initialized correctly
+
+    """
+    self.assertEqual(self.newuser.username,"maureen123")
+
+    self.assertEqual(self.newuser.password,"hey123")
