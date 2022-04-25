7d9ba2f51d6f04a5cfb322c597a5373d12fef1c3
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-25 08:13:21 +0300

    "Feat: added files to folder passwordlocker"

---
+++ passwordlocker/user_test.py
@@ -0,0 +1,45 @@
+import unittest
+from user import User
+
+class Usertest(unittest.TestCase):
+
+    """
+    this class test each case from the user class
+
+    """
+def setUp(self):
+
+    """
+
+    the method setUp runs before each case in the user class
+
+    """
+    self.newuser= User("maureen123","hey123")
+
+def test_init(self):
+
+    """
+    test to confirm if the object user has been initialized correctly
+
+    """
+    self.assertEqual(self.newuser.username,"maureen123")
+
+    self.assertEqual(self.newuser.password,"hey123")
+
+def test_save_user(self):
+    """
+    test to check if user is saved into the userlist dict
+
+    """
+    self.newuser.save_user()
+    self.assertEqual(len(User.userlist),1)
+
+def test_delete_user(self):
+    """
+
+    test to check if user is deleted from the userlist dict
+
+    """
+ 
+if __name__ == "__main__":
+    unittest.main()
