461a5346649a64ec62e801cad8dfd8903758fee1
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-04-25 07:09:01 +0300

    "adds the if statement for the main function"

--- run.py
+++ run.py
@@ -35,3 +35,50 @@ def display_user():
     return Credentials.credential_display()
 
 def main():
+    while True:
+        print("Welcome to your password locker. Key in Sgu -or Lgn for signup or login respectively to continue")
+        print("Sgu -or- Lgn")
+        option = input()
+        if option == "Sgu":
+            print("Please enter a Username:")
+            username=input()
+            print("Please enter desired password:")
+            password=input()
+            save_user(create_newuser(username,password))
+            print("Hello your password locker has been successfully activated")
+            print(f"Username:{username}")
+            print(f"Password:{password}")
+            print("You can now use you details to login")
+        elif option == "Lgn":
+            print("Enter username")
+            username=input()
+            print("Enter password")
+            password=input()
+            if find_user(username):
+                print("Create new account or View existing accounts key in add exist for adding accounts or viewing existing accounts respectively")
+                print(" add -or- exist")
+                selection =input()
+                if selection == "add":
+                    print("Add an account")
+                    print("Enter account name")
+                    accname=input()
+                    accusername=username
+                    print("Create password or Get an automatic password. Enter Cp or Gp for both options respectively")
+                    print("Cp -or- Gp")
+                    choice = input()
+                    if choice == "Cp":
+                        print("Enter password")
+                        accpassword=input()
+                    elif choice == "Gp":
+                        letters = string.ascii_lowercase
+                        accpassword="".join(random.choice(letters) for i range(5))
+                        print(f"Password:{accpassword}")
+                    print(f"Username:{accusername} Account name:{accname} Account password:{accpassword}")
+                elif selection == "exist":
+                    if find_credential(a_name):
+                        print("Here are your accounts:")
+                        for user in credential_display():
+                            print(f"Account:{user.accname}")
+                            print(f"Password:{user.accpassword}")
+if __name__ == "__main__":
+    main()
